b12a3ee0255d9b7f57860d119f4e691929ae3bbe
pcchamikara <pcchamikara@gmail.com>
2023-01-19 15:35:06 +0530

    Added footer

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -1,5 +1,26 @@
 import React from 'react';
+import './footer.scss';
+import bg from '../images/header.jpg';
+import movieDb from '../images/moviedb.svg';
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div
+      className="footer container"
+      style={{
+        background: ` linear-gradient(to bottom, rgba(15, 15, 15, 0.52), rgba(15, 15, 15)),url(${bg})`,
+      }}
+    >
+      <p>
+        <a
+          href="https://pulasthichamikara.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Developed by Pulasthi Chamikara Karunadhipathi
+        </a>
+      </p>
+      <img src={movieDb} width="60" />
+    </div>
+  );
 }
